9ac7b81aef170197353d79280e618efca9d88d47
KoloinaTiana <koloina.rasolofoson@etu.univ-tours.fr>
2023-03-29 02:17:46 +0200

    Recherche par xpath

--- XmlUtils.java
+++ XmlUtils.java
@@ -1,22 +1,55 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.filter.ElementFilter;
+import org.jdom2.filter.Filter;
 import org.jdom2.filter.Filters;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import org.jdom2.xpath.XPathExpression;
 import org.jdom2.xpath.XPathFactory;
 
 public class XmlUtils {
     
-    public List<Element> getElementByXPath(String n) throws Exception {
-        String xPath = "//plat[contains(translate(nom, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '" + n.toLowerCase() + "')]";
-        Document document = new SAXBuilder().build(new File("restaurantCatalogue.xml"));
-        XPathFactory xPathFactory = XPathFactory.instance();
-        XPathExpression<Element> expression = xPathFactory.compile(xPath, Filters.element());
-        System.out.println(xPath);
-        return expression.evaluate(document);
-    }
+    public List<Element> getElementByXPath(String n) {
+        String xPath = "//plat[contains(lower-case(nom), '" + n.toLowerCase() + "')]";
+        Document document;
+        try {
+            document = new SAXBuilder().build(new File("restaurantCatalogue.xml"));
+            XPathFactory xpfac = XPathFactory.instance();
+            XPathExpression<Element> xp = xpfac.compile(xPath, Filters.element());
+        
+            List<Element> elements = xp.evaluate(document);
+            List<Element> filteredElements = new ArrayList<>();
+            for (Element element : elements) {
+                if (element.getName().equals("plat")) {
+                    filteredElements.add(element);
+                }
+            }
+    
+            if (!filteredElements.isEmpty()) {
+                for (Element element : filteredElements) {
+                    String nom = element.getChildText("nom");
+                    System.out.println(nom);
+                }
+            } else {
+                System.out.println("Aucun plat trouvé pour la recherche : " + n);
+            }
+    
+            return filteredElements;
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }     
     
 }
